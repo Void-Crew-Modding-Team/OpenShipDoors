467bbec797079af2c08393e0fdf372957814e0dc
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-03-18 10:54:09 -0700

    -Rebuilt source for VS -Added VoidPlugin.cs

--- MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenShipDoors
-{
-    static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "id107.openshipdoors";
-        public const string PLUGIN_NAME = "OpenShipDoors";
-        public const string PLUGIN_VERSION = "0.0.0";
-    }
-}
---
+++ OpenShipDoors/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace OpenShipDoors
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Client;
+
+        public override string Author => "18107, Dragon";
+
+        public override string Description => "Opens ship doors on game startup";
+    }
+}
